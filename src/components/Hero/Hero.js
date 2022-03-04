598ba28a1eed7b752f4159252769fd575b882d81
Isaac7app <ik.appiagyei@gmail.com>
2022-03-04 09:09:15 +0100

    added a header and a hero section to the portfolio

---
+++ src/components/Hero/Hero.js
@@ -0,0 +1,22 @@
+import React from 'react';
+
+import { Section, SectionText, SectionTitle } from '../../styles/GlobalComponents';
+import Button from '../../styles/GlobalComponents/Button';
+import { LeftSection } from './HeroStyles';
+
+const Hero = () => (
+  <Section row nopadding>
+    <LeftSection>
+      <SectionTitle main center>
+        Hello, Welcome <br />
+        To My Portfolio
+      </SectionTitle>
+      <SectionText>
+        I am web developer that have the passion and dedication to learn and build applications.I am confiendent in my skils and all I ask for is a chance to showcase my ability.
+      </SectionText>
+      <Button onClick={() => window.location = 'https://github.com/Isaac7app'}>More About Me</Button>
+    </LeftSection>
+  </Section>
+);
+
+export default Hero;
