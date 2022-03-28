92ec01a12f33e6ba4ba0fd648be76b072c82ce0a
Isaac7app <ik.appiagyei@gmail.com>
2022-03-28 16:29:57 +0200

    added a new image and also trim some code down

--- src/components/Footer/Footer.js
+++ src/components/Footer/Footer.js
@@ -1,8 +1,18 @@
-import React from 'react';
-import { AiFillGithub, AiFillInstagram, AiFillLinkedin } from 'react-icons/ai';
+import React from "react";
+import { AiFillGithub, AiFillInstagram, AiFillLinkedin } from "react-icons/ai";
 
-import { SocialIcons } from '../Header/HeaderStyles';
-import { CompanyContainer, FooterWrapper, LinkColumn, LinkItem, LinkList, LinkTitle, Slogan, SocialContainer, SocialIconsContainer } from './FooterStyles';
+import { SocialIcons } from "../Header/HeaderStyles";
+import {
+  CompanyContainer,
+  FooterWrapper,
+  LinkColumn,
+  LinkItem,
+  LinkList,
+  LinkTitle,
+  Slogan,
+  SocialContainer,
+  SocialIconsContainer,
+} from "./FooterStyles";
 
 const Footer = () => {
   return (
@@ -10,23 +20,25 @@ const Footer = () => {
       <LinkList>
         <LinkColumn>
           <LinkTitle>Call</LinkTitle>
-          <LinkItem href='tel:0046-768994340'>0046-768994340</LinkItem>
+          <LinkItem href="tel:0046-768994340">0046-768994340</LinkItem>
         </LinkColumn>
         <LinkColumn>
           <LinkTitle>Email</LinkTitle>
-          <LinkItem href='mailto:ik.appiagyei@gmail.com'>ik.appiagyei@gmail.com</LinkItem>
+          <LinkItem href="mailto:ik.appiagyei@gmail.com">
+            ik.appiagyei@gmail.com
+          </LinkItem>
         </LinkColumn>
       </LinkList>
       <SocialIconsContainer>
         <CompanyContainer>
           <Slogan>Innovating and learning one project at a time</Slogan>
         </CompanyContainer>
-            <SocialIcons href="https://github.com/Isaac7app">
-            <AiFillGithub size="3rem" />
-          </SocialIcons>
-          <SocialIcons href="https://linkedin.com/in/isaac-appiagyei-808bb8226">
-            <AiFillLinkedin size="3rem" />
-          </SocialIcons>
+        <SocialIcons href="https://github.com/Isaac7app">
+          <AiFillGithub size="3rem" />
+        </SocialIcons>
+        <SocialIcons href="https://linkedin.com/in/isaac-appiagyei-808bb8226">
+          <AiFillLinkedin size="3rem" />
+        </SocialIcons>
       </SocialIconsContainer>
     </FooterWrapper>
   );
--- src/components/Hero/Hero.js
+++ src/components/Hero/Hero.js
@@ -1,20 +1,30 @@
-import React from 'react';
+import React from "react";
 
-import { Section, SectionText, SectionTitle } from '../../styles/GlobalComponents';
-import Button from '../../styles/GlobalComponents/Button';
-import { LeftSection } from './HeroStyles';
+import {
+  Section,
+  SectionText,
+  SectionTitle,
+} from "../../styles/GlobalComponents";
+import Button from "../../styles/GlobalComponents/Button";
+import { LeftSection } from "./HeroStyles";
 
 const Hero = () => (
   <Section row nopadding>
     <LeftSection>
       <SectionTitle main center>
-        Hello,I'm Isaac  <br />
+        Hello,I'm Isaac <br />
         Welcome To My Portfolio
       </SectionTitle>
       <SectionText>
-        I am web developer that have the passion and dedication to learn and build applications.I am confiendent in my skils and all I ask for is a chance to showcase my ability.
+        I am full-stack web developer, passionate and dedicated to learning and
+        building web applications.I am confiendent in my skils and all I ask for
+        is a chance to showcase my ability.
       </SectionText>
-      <Button onClick={() => window.location = 'https://github.com/Isaac7app'}>More About Me</Button>
+      <Button
+        onClick={() => (window.location = "https://github.com/Isaac7app")}
+      >
+        More About Me
+      </Button>
     </LeftSection>
   </Section>
 );
